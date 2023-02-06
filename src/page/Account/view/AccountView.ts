e9b59644430209da47f815eba1ad0cb660c6c1d3
tizary <tizary@mail.ru>
2023-02-07 00:49:19 +0300

    feat: auth

--- src/page/Account/view/AccountView.ts
+++ src/page/Account/view/AccountView.ts
@@ -21,7 +21,6 @@ class AccountView {
         this.link = this.root.querySelector('.link__block-change');
 
         this.link?.addEventListener('click', this.onLink);
-
         this.changeBlock();
     }
 
@@ -50,9 +49,10 @@ class AccountView {
             <img class="logo__account" src="${logo}">
 
             <form class="form__account">
-                <input class="input user-input__account" type="text" placeholder="Имя Пользователя">
+                <input class="input user-input__account" type="text" placeholder="эл. адрес">
                 <input class="input password-input__account" type="text" placeholder="Пароль">
-                <button class="btn__account">Войти</button>
+                <button class="btn__account" id="btn__login">Войти</button>
+                <div id="divLoginError" class="input"></div>
             </form>
         </div>`;
     }
@@ -64,17 +64,71 @@ class AccountView {
             <p class="text__registration">Зарегистрируйтесь, чтобы смотреть фото и видео ваших друзей.</p>
 
             <form class="form__account">
-                <input class="input email-input__account" type="text" placeholder="эл.адрес">
+                <input class="input email-input__account" type="text" placeholder="эл. адрес">
                 <input class="input name-surname-input__account" type="text" placeholder="Имя и Фамилия">
                 <input class="input name-input__account" type="text" placeholder="Имя пользователя">
                 <input class="input password-input__account" type="text" placeholder="Пароль">
-                <button class="btn__account">Далее</button>
+                <button class="btn__account" id="btn__registr">Далее</button>
             </form>
         </div>`;
     }
 
+    // private userLogin() {
+    //     const loginBtn = document.querySelector('#btn__login');
+    //     const divErr: HTMLFormElement | null = document.querySelector('#divLoginError');
+    //     const email: HTMLInputElement | null = document.querySelector('.user-input__account');
+    //     const password: HTMLInputElement | null = document.querySelector('.password-input__account');
+    //     loginBtn?.addEventListener('click', (e) => {
+    //         e.preventDefault();
+    //         if (divErr) {
+    //             if (email && password) {
+    //                 if (email.value && password.value) {
+    //                     Auth.instance.logIn(email.value, password.value);
+    //                     Auth.instance.monitorAuthState();
+    //                 } else if (!email.value) {
+    //                     divErr.innerHTML = 'Wrong email. Try again.';
+    //                 } else if (!password.value) {
+    //                     divErr.innerHTML = 'Wrong password. Try again.';
+    //                 }
+    //             }
+    //         }
+    //     });
+    //     if (email && divErr) {
+    //         email.onfocus = function () {
+    //             divErr.innerHTML = '';
+    //         };
+    //     }
+    // }
+
+    // private userRegistration() {
+    //     const regBtn = document.querySelector('#btn__registr');
+    //     const email: HTMLInputElement | null = document.querySelector('.email-input__account');
+    //     const name: HTMLInputElement | null = document.querySelector('.name-surname-input__account');
+    //     const nikName: HTMLInputElement | null = document.querySelector('.name-input__account');
+    //     const password: HTMLInputElement | null = document.querySelector('.password-input__account');
+    //     regBtn?.addEventListener('click', (e) => {
+    //         e.preventDefault();
+    //         if (email && name && nikName && password) {
+    //             const user = {
+    //                 email: email.value,
+    //                 name: name.value,
+    //                 nikName: nikName.value,
+    //                 password: password.value,
+    //             };
+    //             console.log(user);
+    //             Auth.instance.setUser(user);
+    //             Auth.instance.authentication(email.value, password.value);
+    //             email.value = '';
+    //             name.value = '';
+    //             nikName.value = '';
+    //             password.value = '';
+    //         }
+    //     });
+    // }
+
     private onLink = () => {
         this.changeBlock();
+        // this.userRegistration();
     };
 
     private changeBlock(): void {
@@ -87,6 +141,7 @@ class AccountView {
 
                 this.container.innerHTML = '';
                 this.container.insertAdjacentHTML('afterbegin', this.makeLogin());
+                // this.userLogin();
                 this.stateBlockChange = StateBlockChangeAccount.REGISTRATION;
                 break;
 
