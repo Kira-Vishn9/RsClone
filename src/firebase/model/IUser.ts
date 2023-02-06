19c03f3135a2ccbb28c09071cb83884f8e9c46d6
Николай <Doonn3@gmail.com>
2023-02-06 23:04:48 +0300

    feat: add fields

--- src/firebase/model/IUser.ts
+++ src/firebase/model/IUser.ts
@@ -1,9 +1,20 @@
+import { DocumentData } from 'firebase/firestore/lite';
+
 interface IUser {
     email: string;
     name: string;
     surname: string;
     nikName: string;
     password: string;
+    followers?: DocumentData;
+    followersCount?: number;
+    follower?: DocumentData;
+    followerCount?: number;
+    posts?: DocumentData;
+}
+
+interface IFollower {
+    //
 }
 
 export default IUser;
