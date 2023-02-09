1ec6ecfd78d76c77f3fd2f55d412ae2cc0e500da
Николай <Doonn3@gmail.com>
2023-02-09 23:50:55 +0300

    refactor: clear

--- src/firebase/auth/Auth.ts
+++ src/firebase/auth/Auth.ts
@@ -12,6 +12,7 @@ import IUser from '../model/IUser';
 import { FirebaseError } from 'firebase/app';
 import { LocalStorage } from '../../localStorage/localStorage';
 import userState from '../../state/user.state';
+import UserService from '../service/UserSevice';
 
 class Auth {
     public static instance = new Auth();
@@ -20,87 +21,12 @@ class Auth {
     private data = collection(this.db, 'Users');
     private auth = getAuth();
 
-    // Вернуть массив юзеров
-    private async getAllUser(): Promise<IUser[] | boolean> {
-        try {
-            const collections = await getDocs(this.data);
-            const data: IUser[] = collections.docs.map((doc) => doc.data()) as IUser[];
-            return data;
-        } catch (error: unknown) {
-            console.error(error);
-            return false;
-        }
-    }
-
-    public async getUser(id: string): Promise<IUser> {
-        const docRef = doc(this.data, id);
-        const result = await getDoc(docRef);
-        const tt = result.data() as IUser;
-        return tt;
-    }
-
-    // Вернуть юзера или null
-    private async getUser(email: string, password: string): Promise<IUser> {
-        // const datasSet = collection(this.db, 'DatasSet');
-        // const dataSnapshot = await getDocs(datasSet);
-        // const dataSnapshot = await getDoc(datasSet.path);
-        // const dataList = dataSnapshot.docs.map((doc) => doc.data());
-        // console.log(dataList);
-        // console.log(dataSnapshot)
-        // const docRef = doc(this.data, 'xPEZiSFKJZpeMVHddnQS');
-        // const docSnap = await getDoc(docRef);
-        // docSnap.
-        // const bb = docSnap.data();
-        // const tt = await getDoc(bb);
-        // console.log(docSnap.ref);
-        const collections = await getDocs(this.data);
-
-        const docData = collections.docs.map((doc) => doc.data());
-        const getDoc1 = docData.filter((doc) => doc.email === email);
-        const user = getDoc1[0] as IUser;
-        // const user = getDoc1[0];
-        // const posts = user.posts as DocumentData;
-        // console.log(posts.id);
-        return user;
-        // const bebe = getDoc.map((doc) => doc.data())
-        // console.log(getDoc.map((doc) => console.log(doc)));
-        // const docRef = doc(this.data, 'Tod');
-
-    //     // const docRef = doc(this.data, 'xPEZiSFKJZpeMVHddnQS');
-    //     // const col = collection(this.db, 'Posts');
-    //     // const docRef = doc(col, posts.id);
-    //     // const docSnap = await getDoc(docRef);
-    //     // console.log(docSnap.data());
-    //     // const docSnap = await getDoc(docRef);
-    //     // const snap = docSnap.data();
-    //     // console.log(snap);
-    // }
-
-    // записать юзера
-    private async setUser(user: IUser): Promise<void> {
-        const data = collection(this.db, 'Users');
-        const docRef = doc(data, id);
-        console.log('setUserID: ' + docRef.id);
-        const userStore = LocalStorage.instance.getUser();
-        userStore.id = docRef.id;
-        // userState.id = docRef.id;
-        await setDoc(docRef, user);
-    }
-
-    // Обновить данные юзера
-    private async updateUser(): Promise<void> {
-        //
-    }
-
     public async signupUser(user: IUser): Promise<string | undefined> {
         const email = user.email;
         const pass = user.password;
         try {
             const create = await createUserWithEmailAndPassword(this.auth, email, pass);
-            this.setUser(user);
-            // const authUser = create.user;
-            // console.log(authUser);
-            // return user;
+            UserService.instance.setUser(create.user.uid, user);
             return '';
         } catch (error) {
             if (error instanceof FirebaseError) {
@@ -108,20 +34,6 @@ class Auth {
             }
         }
     }
-
-    // public async signinUser(user: IUser): Promise<boolean | IUser> {
-    //     const email = user.email;
-    //     const pass = user.password;
-    //     try {
-    //         const create = await signInWithEmailAndPassword(this.auth, email, pass);
-    //         const authUser = create.user;
-    //         console.log(authUser);
-    //         return user;
-    //     } catch (error: unknown) {
-    //         console.log(error);
-    //         return false;
-    //     }
-    // }
 
     public test: IUser | null = null;
     public async signinUser(email: string, password: string) {
@@ -156,4 +68,5 @@ class Auth {
         });
     }
 }
+
 export default Auth;
