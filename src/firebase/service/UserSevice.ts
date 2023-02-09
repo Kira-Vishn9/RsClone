96745fb61543bf19198b252b15341d724d20988e
Николай <Doonn3@gmail.com>
2023-02-09 20:05:52 +0300

    feat: add UserService

---
+++ src/firebase/service/UserSevice.ts
@@ -0,0 +1,61 @@
+import { collection, doc, getDoc, getDocs, getFirestore, setDoc } from 'firebase/firestore/lite';
+import userState from '../../state/user.state';
+import UserState from '../../state/UserState';
+import app from '../config/config';
+import IUser from '../model/IUser';
+
+class UserService {
+    public static instance: UserService = new UserService();
+
+    private db = getFirestore(app);
+    private data = collection(this.db, 'Users');
+    private constructor() {
+        //
+    }
+
+    // Вернуть массив юзеров
+    private async getAllUser(): Promise<IUser[] | boolean> {
+        try {
+            const collections = await getDocs(this.data);
+            const data: IUser[] = collections.docs.map((doc) => doc.data()) as IUser[];
+            return data;
+        } catch (error: unknown) {
+            console.error(error);
+            return false;
+        }
+    }
+    // Вернуть юзера или null
+    public async getUser(id: string): Promise<IUser | null> {
+        try {
+            const docRef = doc(this.data, id);
+            const docSnaphot = await getDoc(docRef);
+            const result = docSnaphot.data() as IUser;
+            return result;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    }
+
+    // записать юзера
+    public async setUser(id: string, user: IUser): Promise<void> {
+        try {
+            // const docRef = doc(this.data);
+            const data = collection(this.db, 'Users');
+            const docRef = doc(data, id);
+            console.log('setUserID: ' + docRef.id);
+            userState.id = docRef.id;
+            UserState.instance.setUserID(docRef.id);
+            await setDoc(docRef, user);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Обновить данные юзера
+    private async updateUser(): Promise<void> {
+        //
+    }
+}
+
+export default UserService;
