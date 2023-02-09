6fba5474e4363eca46fc441b034d9607c2f66a10
Николай <Doonn3@gmail.com>
2023-02-09 20:07:25 +0300

    refactor: popup

--- src/page/aside/popapUpload.ts
+++ src/page/aside/popapUpload.ts
@@ -1,23 +1,43 @@
 import './popapUpload.scss';
 
 function createPopap() {
+    //   const popap = `
+    //   <div class="popap" id="popap-upload">
+    //     <div class="popap-dark">
+    //       <div class="popap-window">
+    //         <div class="upload-content">
+    //           <div class="upload-content__header">Create a post</div>
+    //           <div class="upload-content__main">
+    //             <span class="upload-content__main-img"></span>
+    //             <button class="upload-content__main-btn">Select on computer</button>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // `;
+
     const popap = `
-    <div class="popap" id="popap-upload">
-      <div class="popap-dark">
-        <div class="popap-window">
-          <div class="upload-content">
-            <div class="upload-content__header">Create a post</div>
-            <div class="upload-content__main">
-              <span class="upload-content__main-img"></span>
-              <button class="upload-content__main-btn">Select on computer</button>
-            </div>
+  <div class="popap" id="popap-upload">
+    <div class="popap-dark">
+      <div class="popap-window">
+        <div class="upload-content">
+          <div class="upload-content__header">Create a post</div>
+          <div class="upload-content__main">
+            <span class="upload-content__main-img"></span>
+            <input type="file" class="upload-content__main-btn">Select on computer</input>
           </div>
         </div>
       </div>
     </div>
-  `;
-
+  </div>
+`;
     document.body.insertAdjacentHTML('beforeend', popap);
+    document.querySelector('.upload-content__main-btn')?.addEventListener('click', () => {
+        // pushkin('#file', {
+        //   multi: true,
+        //   accept: ['.png', '.jpg', '.jpeg', '.gif']})
+    });
     document.body.classList.add('covert');
 
     const popapUpload = document.querySelector('#popap-upload') as HTMLElement;
@@ -30,5 +50,31 @@ function createPopap() {
         }
     });
 }
+
+// Doonn >> Refactrorng
+function newPost(src: string): string {
+    return `
+      <div class="upload-content__header">Create a post</div>
+      <div class="upload-content__main">
+        <img class = 'img-post' src="${src}" 
+        style="
+          margin-bottom: 20px;
+          width: 90%;
+      alt="">
+        <input tupe='text' style="
+        height: 200px;
+        background: black;
+        width: 90%;
+        color: #fff;
+        border: 0px solid black;
+        border-radius: 10px;
+        "
+        >
+        <input type="file" id="file">
+        <button class="submit upload-content__main-btn">Submit</button>
+      </div>
+    `;
+}
+// << End Doonn
 
 export default createPopap;
