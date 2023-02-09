e0808affe27d40c54cde4f8fa158334cb79470ba
222 <kiravishn9@gmail.com>
2023-02-09 16:24:08 -0500

    feat: vodal weindow

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import App from './app/app';
 import './shared/style/main.scss';
+import OpenImg from './profileOpenImg/OpenImg';
 
 const app = new App();
 app.init();
+const open = new OpenImg();
+document.querySelector('body')?.insertAdjacentHTML( 'afterbegin' , open.Overflow());
