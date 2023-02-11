74c79fcdfe286cde35aafa72ff333ab1636eb052
222 <kiravishn9@gmail.com>
2023-02-10 19:19:17 -0500

    fix:

--- webpack.config.js
+++ webpack.config.js
@@ -8,68 +8,72 @@ const devMode = mode === 'development';
 const target = devMode ? 'web' : 'browserslist';
 const devtool = devMode ? 'source-map' : undefined;
 module.exports = {
-    mode,
-    target,
-    devtool,
-    devServer: {
-        port: 3000,
-        historyApiFallback: true,
-        contentBase: './src',
-    },
+	mode,
+	target,
+	devtool,
+	devServer: {
+		port: 3000,
+		historyApiFallback: true,
+		contentBase: "./src",
+	},
     entry: path.resolve(__dirname, './src/index.ts'),
-    output: {
-        filename: '[name].js',
-        publicPath: '/',
-        path: path.resolve(__dirname, 'dist'),
-    },
+	output: {
+		filename: '[name].js',
+		publicPath: "/",
+		path: path.resolve(__dirname, 'dist'),
+	},
     resolve: {
-        extensions: ['.ts', '.js', 'd.ts'],
+        extensions: ['.ts', '.js'],
     },
-    plugins: [
+	plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
         }),
-        new MiniCssExtractPlugin({
-            filename: 'main.css',
-        }),
+		new MiniCssExtractPlugin({
+			filename: 'main.css',
+		}),
         new EslingPlugin({ extensions: 'ts' }),
-    ],
-    module: {
-        rules: [
+	],
+	module: {
+		rules: [
             {
-                test: /\.html$/i,
-                loader: 'html-loader',
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            { test: /\.ts$/i, use: 'ts-loader' },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                    'postcss-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        'postcss-preset-env',
-                                        {
-                                            // Options
-                                        },
-                                    ],
-                                ],
-                            },
-                        },
-                    },
-                ],
-            },
-        ],
-    },
+				test: /\.ts$/i,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.html$/i,
+				loader: 'html-loader',
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+					'postcss-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: [
+									[
+										'postcss-preset-env',
+										{
+											// Options
+										},
+									],
+								],
+							},
+						},
+					},
+				],
+			},
+		],
+	},
 };
