c0d4e71bed66bcc534905c620fe00c4d1b075898
Николай <Doonn3@gmail.com>
2023-02-12 22:31:32 +0300

    refactor: ProfileView

--- src/page/Profile/view/ProfileView.ts
+++ src/page/Profile/view/ProfileView.ts
@@ -1,15 +1,20 @@
 import Observer from '../../../app/observer/Observer';
 import IPosts from '../../../firebase/model/IPosts';
 import IUser from '../../../firebase/model/IUser';
+import ProfileHeadComponent from '../components/ProfileHeadComponent';
 import '../style/profile.scss';
-import dataProfile from '../ui/data-profile';
-import dataProfileTop from '../ui/data.profile.top';
 import makePost from '../ui/item-profile';
 
 class ProfileView {
     private $observer: Observer;
     private root: HTMLElement | null = null;
     private postContainer: HTMLElement | null = null;
+
+    private profileHead: ProfileHeadComponent = new ProfileHeadComponent();
+
+    public get Root() {
+        return this.root;
+    }
 
     public constructor(observer: Observer) {
         this.$observer = observer;
@@ -18,27 +23,26 @@ class ProfileView {
     public init(): void {
         this.root = document.querySelector('.profile');
         if (this.root === null) return;
+        this.profileHead.init(this.root);
 
         this.postContainer = this.root.querySelector('.items-grid__profile');
-        this.$observer.subscribe('eventPost', this.onPost);
+
+        this.$observer.subscribe('eventUser', this.onGetUser);
+        this.$observer.subscribe('eventPost', this.onGetPost);
+        this.profileHead.InputAvatar?.addEventListener('change', this.onChangeAvatar);
+        this.profileHead.BtnSettings?.addEventListener('click', this.onSettings);
     }
 
     public unmount(): void {
-        this.$observer.unsubscribe('eventPost', this.onPost);
+        this.$observer.unsubscribe('eventUser', this.onGetUser);
+        this.$observer.unsubscribe('eventPost', this.onGetPost);
     }
 
     public make(): string {
         return `
             <section class="profile">
 
-                <div class="data__profile">
-                    <img src="https://kipmu.ru/wp-content/uploads/jptr-1.jpg">
-                    <div>
-                        ${dataProfileTop('Jupiter')}
-                        ${dataProfile(154, 147, 577)}
-                        <span>Jupiter Sprogiba</span>
-                    </div>
-                </div>
+                ${this.profileHead.make()}
 
                 <div class="items-grid__profile">
                     
@@ -47,20 +51,37 @@ class ProfileView {
         `.trim();
     }
 
-    private onPost = (event: IPosts) => {
+    private onGetUser = (event: IUser) => {
+        const fullname = event.name;
+        const nickName = event.nikName;
+        this.profileHead.changeFullName(fullname);
+        console.log('FULLNAME: ' + fullname);
+        this.profileHead.changeNickName(nickName);
+
+        if (event.avatar !== undefined) this.profileHead.changeAvatar(event.avatar);
+    };
+
+    private onGetPost = (event: IPosts) => {
         console.log('ProfileView');
         console.log(event);
-        debugger;
 
-        const { fileURL } = event;
-        // const test: string = event.fileURL;
-        // const tt: IPosts = event;
-        // const blabla = tt.fileURL;
-        // debugger;
-        const createPost = makePost(fileURL);
-        // debugger;
-        console.log(createPost);
+        const createPost = makePost(event.fileURL);
+        // this.profileHead?.changeName(event.author.nickName);
+        // this.profileHead.changeFullName(event.author.fullname);
         this.postContainer?.insertAdjacentHTML('afterbegin', createPost);
+    };
+
+    private onChangeAvatar = (event: Event) => {
+        if (!(event.target instanceof HTMLInputElement)) return;
+        const file = event.target.files;
+        if (file === null) return;
+        this.$observer.emit('eventChangeAvatar', file[0], (arg: string) => {
+            this.profileHead.changeAvatar(arg);
+        });
+    };
+
+    private onSettings = () => {
+        window.location.href = '#/settings';
     };
 }
 
