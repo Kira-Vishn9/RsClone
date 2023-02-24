0fea24263e988138af393c289e04292111d2f276
Николай <Doonn3@gmail.com>
2023-02-24 22:35:04 +0300

    refactor: model

--- src/page/Message/model/MessageModel.ts
+++ src/page/Message/model/MessageModel.ts
@@ -2,10 +2,12 @@ import { DataSnapshot } from 'firebase/database';
 import { DocumentSnapshot } from 'firebase/firestore/lite';
 import EventBus from '../../../app/event_bus/EventBus';
 import Observer from '../../../app/observer/Observer';
+import IChatRoom from '../../../firebase/model/IChatRoom';
 import IFollower from '../../../firebase/model/IFollower';
 import IMessage from '../../../firebase/model/IMessage';
 import INewMessage from '../../../firebase/model/INewMessage';
 import ISubscription from '../../../firebase/model/ISubscription';
+import IUser from '../../../firebase/model/IUser';
 import ChatServiсe from '../../../firebase/service/ChatServiсe';
 import FollowersService from '../../../firebase/service/FollowersService';
 import SubscriptionsService from '../../../firebase/service/SubscriptionsService';
@@ -13,6 +15,8 @@ import UserService from '../../../firebase/service/UserSevice';
 import UserState from '../../../state/UserState';
 import ChatComponent from '../component/ChatComponents';
 import EventType from '../type/EventType';
+import RecipientRoom from '../type/RecipientRoom';
+import RecipientStartDialog from '../type/RecipientStartDialog';
 import SubFolType from '../type/SubFolType';
 
 class MessageModel {
@@ -24,124 +28,140 @@ class MessageModel {
     }
 
     public init() {
-        this.getRooms();
+        this.getChatRooms();
+        this.$observer.subscribe(EventType.INIT_OPEN_MODAL, this.onGetFollowsAndSubscriptions);
         this.$observer.subscribe(EventType.CREATE_CHAT_ROOM, this.onCreateChatRoom);
-        this.$observer.subscribe(EventType.openModal, this.getFollowsAndSubscriptions);
-        this.$observer.subscribe(EventType.SEND_MESSAGE, this.onHandlerMessage);
-        this.$observer.subscribe(EventType.START_DIALOG, this.onStartDialog);
+        this.$observer.subscribe(EventType.START_DIALOG, this.onGetRoomID);
+        this.$observer.subscribe(EventType.SEND_MESSAGE, this.onSendMessage);
         EventBus.instance.subscribe(EventBus.instance.eventType.LOAD_MESSAGE, this.onLoadMessage);
     }
 
     public unmount(): void {
+        this.$observer.unsubscribe(EventType.INIT_OPEN_MODAL, this.onGetFollowsAndSubscriptions);
         this.$observer.unsubscribe(EventType.CREATE_CHAT_ROOM, this.onCreateChatRoom);
-        this.$observer.unsubscribe(EventType.openModal, this.getFollowsAndSubscriptions);
-        this.$observer.unsubscribe(EventType.SEND_MESSAGE, this.onHandlerMessage);
-        this.$observer.unsubscribe(EventType.START_DIALOG, this.onStartDialog);
+        this.$observer.unsubscribe(EventType.START_DIALOG, this.onGetRoomID);
+        this.$observer.unsubscribe(EventType.SEND_MESSAGE, this.onSendMessage);
         EventBus.instance.unsubscribe(EventBus.instance.eventType.LOAD_MESSAGE, this.onLoadMessage);
     }
 
-    // Получает Всех Фоловеров для модального окна в Message
-    // eslint-disable-next-line prettier/prettier
-    private getFollowsAndSubscriptions = async (empty: object, cb?: (subFol: SubFolType[]) => void) => {
-        const myUserId = UserState.instance.UserID;
-        if (myUserId === null) return;
-        const myFollowers = await FollowersService.instance.getFollowers(myUserId);
-        const mySubscriptions = await SubscriptionsService.instance.getSubscriptions(myUserId);
+    // Получаем все чат комнаты при инициализации когда обновляем браузер
+    private async getChatRooms(): Promise<void> {
+        const ownUserID = UserState.instance.UserID;
+        console.log(ownUserID);
+        if (ownUserID === null) return;
+        const rooms = await ChatServiсe.instance.getAllChatRoomBySelfUserID(ownUserID);
+        const users = await UserService.instance.getAllUser();
+        if (rooms === null || users === null) return;
+        console.log(rooms);
+        console.log(users);
+        const filterUser = users.filter((user) => user.id !== ownUserID);
 
-        const args: SubFolType[] = [];
+        const data: RecipientRoom[] = [];
 
-        if (myFollowers !== null) {
-            args.push(...myFollowers);
-        }
+        for (let i = 0; i < filterUser.length; i++) {
+            const user = filterUser[i];
 
-        if (mySubscriptions !== null) {
-            args.push(...mySubscriptions);
-        }
+            for (let t = 0; t < rooms.length; t++) {
+                const room = rooms[t];
+                if (room.userID === user.id || room.recipientID === user.id) {
+                    const obj: RecipientRoom = {
+                        recipientUser: user,
+                        room: room,
+                    };
 
-        if (args === undefined) return;
-        for (let i = 0; i < args.length; i += 1) {
-            const elem = args[i];
-            const nextIndex = i + 1;
-            if (nextIndex >= args.length) break;
-            if (elem.userID === args[nextIndex].userID) {
-                args.splice(i, 1);
-                i = 0;
+                    data.push(obj);
+                    break;
+                }
+                // if (user.id === room.recipientID) {
+                //     const obj: RecipientRoom = {
+                //         recipientUser: user,
+                //         room: room,
+                //     };
+
+                //     data.push(obj);
+                //     break;
+                // }
             }
         }
 
-        if (cb !== undefined) {
-            cb(args);
-        }
-    };
+        console.log(data);
 
-    // Создает комнуту для чата когда нажимает кнопку далее в модалке в Message
-    private onCreateChatRoom = async (data: { firstID: string; secondID: string }) => {
-        // this.userId = data.userID;
-        // this.recipienId = data.recipientId;
-        // this.resipientName = data.recipientName;
-        // this.avatarsUrl = data.recipientAvatar;
-
-        await ChatServiсe.instance.createChatRoom(data.firstID, data.secondID);
-        this.chatRoomID = ChatServiсe.instance.RoomID;
-        if (this.chatRoomID !== null) {
-            // ChatServiсe.instance.loadMessage(this.onWatchMessage);
-        }
-        await this.getUser(data.secondID);
-    };
-
-    private async getUser(recipientID: string): Promise<void> {
-        const user = await UserService.instance.getUser(recipientID);
-        this.$observer.emit(EventType.INIT_CHAT_ROOM, user);
+        this.$observer.emit(EventType.INIT_GET_ALL_CHAT__ROOM, data);
     }
-    // << END
 
-    // Получить все Комнаты чатов где есть self user id
-    private async getRooms(): Promise<void> {
-        const id = UserState.instance.UserID;
-        if (id === null) return;
-        const rooms = await ChatServiсe.instance.getAllChatRoomBySelfUserID(id);
-        if (rooms === null) return;
-        const allRecipientUser = rooms.map((user) => {
-            if (user.userID === id) {
-                return user.recipientID;
-            } else if (user.recipientID === id) {
-                return user.userID;
+    // Получает Всех Фоловеров и Подписки для модального окна в Message
+    private onGetFollowsAndSubscriptions = async (
+        empty: object,
+        cb?: (data: (ISubscription & IFollower)[]) => void
+    ) => {
+        const ownUserId = UserState.instance.UserID;
+        if (ownUserId === null) return;
+
+        const getFollowers = await FollowersService.instance.getFollowers(ownUserId);
+        const getSubscriptions = await SubscriptionsService.instance.getSubscriptions(ownUserId);
+
+        let filter: (ISubscription & IFollower)[] = [];
+
+        if (getFollowers !== null) {
+            filter.push(...getFollowers);
+        }
+
+        if (getSubscriptions !== null) {
+            filter.push(...getSubscriptions);
+        }
+
+        console.log(filter);
+
+        filter = filter.filter((val) => val.id !== ownUserId);
+
+        if (filter) {
+            if (cb !== undefined) {
+                cb(filter);
             }
+        }
+        // console.log(filter);
+        // console.log(getFollowers);
+        // console.log(getSubscriptions);
+    };
+
+    // Создает комнаты для чата
+    private onCreateChatRoom = (idArr: string[]) => {
+        const ownUserID = UserState.instance.UserID;
+        if (ownUserID === null) return;
+
+        idArr.forEach(async (id) => {
+            await ChatServiсe.instance.createChatRoom(ownUserID, id);
+            ChatServiсe.instance.recieveEventGetRoom = this.recive;
         });
+    };
 
-        const allUser = await UserService.instance.getAllUser();
+    // Отправлем сразу во вьюшку данные комнаты после создания комнаты
+    private recive = async (room: IChatRoom) => {
+        const recipientUser = await UserService.instance.getUser(room.recipientID);
+        if (recipientUser === null) return;
+        const data: RecipientRoom = {
+            recipientUser: recipientUser,
+            room: room,
+        };
+        this.$observer.emit(EventType.GET_CHAT_ROOM, data); // Event
+        console.log('Получил ЭВЕНТ');
+    };
 
-        if (allUser !== null) {
-            const find = allUser.filter((user) => {
-                if (user.id === undefined) return;
-                return allRecipientUser.includes(user.id);
-            });
+    // запоминаем ид команты
+    private onGetRoomID = (roomID: RecipientStartDialog) => {
+        this.chatRoomID = roomID.roomID;
+        console.log('SAVE ROOM ID:: ', this.chatRoomID);
+    };
 
-            const findObj = {
-                users: find,
-                rooms: rooms,
-            };
-            this.$observer.emit(EventType.GET_ALL_CHAT_ROOM, findObj);
-        }
-    }
-
-    // Срабатывает когда отправляем сообщение в Message
-    private onHandlerMessage = async (message: string) => {
+    // Отпровлчем сообщения
+    private onSendMessage = async (message: string) => {
         if (this.chatRoomID === null) return;
-
         await ChatServiсe.instance.saveMessage(this.chatRoomID, message);
     };
 
-    private onStartDialog = async (roomID: string) => {
-        this.chatRoomID = roomID;
-        const messageArr = await ChatServiсe.instance.getAllMessagesByRoomID(roomID);
-        if (messageArr === null) return;
-        // cb(messageArr);
-        this.$observer.emit(EventType.GET_ALL_MESSAGE_START_DIALOG, messageArr);
-        console.log(messageArr);
-    };
-
+    // Отлавливаем сообщения в реальном времени
     private onLoadMessage = (message: INewMessage) => {
+        console.log('RECIEVE_MESSAGE::', message);
         this.$observer.emit(EventType.RECEIVE_MESSAGE, message);
     };
 }
