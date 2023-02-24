02f867bda057cbe86de9a61abf48aaded09e3bed
Николай <Doonn3@gmail.com>
2023-02-24 22:35:59 +0300

    refactor: type

--- src/page/Message/type/SubFolType.ts
+++ src/page/Message/type/SubFolType.ts
@@ -1,6 +1,9 @@
 import IFollower from '../../../firebase/model/IFollower';
 import ISubscription from '../../../firebase/model/ISubscription';
 
-type SubFolType = ISubscription & IFollower;
+type SubFolType = {
+    subs: ISubscription[];
+    follows: IFollower[];
+};
 
 export default SubFolType;
