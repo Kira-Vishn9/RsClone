269f9d4d9dc2c81fdd22c25cebaa2ec23ba7b9cf
Николай <Doonn3@gmail.com>
2023-02-26 23:18:17 +0300

    refactor: change config

--- src/firebase/config/config.ts
+++ src/firebase/config/config.ts
@@ -10,15 +10,15 @@ import { initializeApp } from 'firebase/app';
 //     appId: '1:32448233635:web:304c3f503ac8f0d4a5ff16',
 // };
 
-const firebaseConfig = {
-    apiKey: 'AIzaSyAbrSlPJqm6a0sdSm-f4Ds7MWrIpoyIF-s',
-    authDomain: 'clone-instagram-7c300.firebaseapp.com',
-    databaseURL: 'https://clone-instagram-7c300-default-rtdb.firebaseio.com',
-    projectId: 'clone-instagram-7c300',
-    storageBucket: 'clone-instagram-7c300.appspot.com',
-    messagingSenderId: '572673925162',
-    appId: '1:572673925162:web:c129d35a8ea48e1ce9a353',
-};
+// const firebaseConfig = {
+//     apiKey: 'AIzaSyAbrSlPJqm6a0sdSm-f4Ds7MWrIpoyIF-s',
+//     authDomain: 'clone-instagram-7c300.firebaseapp.com',
+//     databaseURL: 'https://clone-instagram-7c300-default-rtdb.firebaseio.com',
+//     projectId: 'clone-instagram-7c300',
+//     storageBucket: 'clone-instagram-7c300.appspot.com',
+//     messagingSenderId: '572673925162',
+//     appId: '1:572673925162:web:c129d35a8ea48e1ce9a353',
+// };
 
 // const firebaseConfig = {
 //     apiKey: 'AIzaSyBUzYL3IZqUD3gKEkhoeQesm5m6fkitS9Y',
@@ -29,6 +29,17 @@ const firebaseConfig = {
 //     messagingSenderId: '238045707767',
 //     appId: '1:238045707767:web:94f7fa1a43cf21a25502f2',
 // };
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBArE9ymJXKuYsS8_DOsar1Ebb5n29Byc8',
+    authDomain: 'my-last-project-30f8a.firebaseapp.com',
+    databaseURL: 'https://my-last-project-30f8a-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'my-last-project-30f8a',
+    storageBucket: 'my-last-project-30f8a.appspot.com',
+    messagingSenderId: '20021421397',
+    appId: '1:20021421397:web:a89788389263b370053bdc',
+    measurementId: 'G-22K4SGNZG8',
+};
 
 const app = initializeApp(firebaseConfig);
 export default app;
