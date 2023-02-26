6f77ad200e368f430f27f5723cc5c7188f540982
Николай <Doonn3@gmail.com>
2023-02-26 22:56:00 +0300

    refactor: change config

--- src/firebase/config/config.ts
+++ src/firebase/config/config.ts
@@ -10,25 +10,25 @@ import { initializeApp } from 'firebase/app';
 //     appId: '1:32448233635:web:304c3f503ac8f0d4a5ff16',
 // };
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAbrSlPJqm6a0sdSm-f4Ds7MWrIpoyIF-s",
-//     authDomain: "clone-instagram-7c300.firebaseapp.com",
-//     databaseURL: "https://clone-instagram-7c300-default-rtdb.firebaseio.com",
-//     projectId: "clone-instagram-7c300",
-//     storageBucket: "clone-instagram-7c300.appspot.com",
-//     messagingSenderId: "572673925162",
-//     appId: "1:572673925162:web:c129d35a8ea48e1ce9a353"
-//   };
-
 const firebaseConfig = {
-    apiKey: 'AIzaSyBUzYL3IZqUD3gKEkhoeQesm5m6fkitS9Y',
-    authDomain: 'rs-clone-insta.firebaseapp.com',
-    databaseURL: 'https://rs-clone-insta-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'rs-clone-insta',
-    storageBucket: 'rs-clone-insta.appspot.com',
-    messagingSenderId: '238045707767',
-    appId: '1:238045707767:web:94f7fa1a43cf21a25502f2',
+    apiKey: 'AIzaSyAbrSlPJqm6a0sdSm-f4Ds7MWrIpoyIF-s',
+    authDomain: 'clone-instagram-7c300.firebaseapp.com',
+    databaseURL: 'https://clone-instagram-7c300-default-rtdb.firebaseio.com',
+    projectId: 'clone-instagram-7c300',
+    storageBucket: 'clone-instagram-7c300.appspot.com',
+    messagingSenderId: '572673925162',
+    appId: '1:572673925162:web:c129d35a8ea48e1ce9a353',
 };
+
+// const firebaseConfig = {
+//     apiKey: 'AIzaSyBUzYL3IZqUD3gKEkhoeQesm5m6fkitS9Y',
+//     authDomain: 'rs-clone-insta.firebaseapp.com',
+//     databaseURL: 'https://rs-clone-insta-default-rtdb.europe-west1.firebasedatabase.app',
+//     projectId: 'rs-clone-insta',
+//     storageBucket: 'rs-clone-insta.appspot.com',
+//     messagingSenderId: '238045707767',
+//     appId: '1:238045707767:web:94f7fa1a43cf21a25502f2',
+// };
 
 const app = initializeApp(firebaseConfig);
 export default app;
